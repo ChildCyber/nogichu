dab923b0cd27d631b6d675206d32d5dda35f4c38
Mr.Child <lishunsheng2012@gmail.com>
2020-01-02 19:19:31 +0800

    add config.js

---
+++ common/config.js
@@ -0,0 +1,14 @@
+// 动态配置信息
+const uri = "mongodb://localhost:27017";
+const dbName = "nogi";
+const email = {
+    service: "QQex",
+    user: "",
+    pass: "",
+};
+
+module.exports = {
+    mongoUrl: uri,
+    dbName: dbName,
+    email: email
+};
