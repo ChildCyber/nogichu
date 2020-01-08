48d51332562e3aada65b63c24367c2d4edcd787a
Mr.Child <lishunsheng2012@gmail.com>
2020-01-08 21:43:04 +0800

    add pay-alipay page

--- routes/pay.js
+++ routes/pay.js
@@ -6,6 +6,53 @@ const moment = require('moment');
 moment.locale('zh-cn');
 
 /**
+ * 支付宝支付
+ */
+router.get('/pay-alipay', Auth.notLoggedIn, (req, res) => {
+    const db = req.app.locals.db;
+
+    db.collection('premium').findOne({'phone': req.session.phone, 'user': req.session.user})
+        .then(data => {
+            if (data) {
+                let now = new Date();
+                if (now < data.expired_at) {
+                    return res.redirect('/mypage');
+                } else {
+                    let expiredAt = new Date(now.getTime());
+                    expiredAt.setFullYear(expiredAt.getFullYear() + 1);
+                    return db.collection('premium').updateOne({
+                        'phone': req.session.phone,
+                        'user': req.session.user
+                    }, {$set: {'expired_at': expiredAt}})
+                }
+            } else {
+                let createdAt = new Date();
+                let expiredAt = new Date(createdAt.getTime());
+                expiredAt.setFullYear(expiredAt.getFullYear() + 1);
+                return db.collection('premium').insertOne({
+                    'phone': req.session.phone,
+                    'user': req.session.user,
+                    'created_at': createdAt,
+                    'expired_at': expiredAt,
+                    'point': 0,
+                    'id': util.randomString()
+                });
+            }
+        })
+        .then(data => {
+            if (data) {
+                console.log(data.ops[0]);
+                req.session.premium = data.ops[0];
+                res.redirect('/mypage');
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.send('充值失败，请稍后重试')
+        });
+});
+
+/**
  * 支付
  */
 router.get('/pay', Auth.notLoggedIn, (req, res) => {
