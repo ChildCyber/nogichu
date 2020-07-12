d0a8d96d39c6b053f9ee7b3babe7bd632fda3f9c
Mr.Child <lishunsheng2012@gmail.com>
2020-07-12 14:41:44 +0800

    update blog detail

--- routes/blog.js
+++ routes/blog.js
@@ -5,14 +5,28 @@ const router = express.Router();
  * 新闻资讯
  */
 router.get('/', (req, res) => {
+    // todo 分页
+    let page = req.query.page || 0;
     res.render('blog/blog.ejs', req.ev);
 });
 
 /**
  * 资讯详情
  */
-router.get(/.*/, (req, res) => {
-    res.render('blog/blog-detail.ejs', req.ev);
+router.get('/:slug', (req, res) => {
+    const db = req.app.locals.db;
+    db.collection('blog').findOne({'slug': req.params.slug})
+        .then(data => {
+            if (data) {
+                res.render('blog/blog-detail.ejs', data);
+            } else {
+                res.status(404).render('404');
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.send('程序异常，请稍后重试');
+        });
 });
 
 module.exports = router;
